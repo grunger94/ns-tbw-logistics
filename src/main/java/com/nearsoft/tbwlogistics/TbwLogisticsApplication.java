d8112df8f515fd4d0e7db4d12cddcbe328571c83
Rafa Manrique <rmanrique@nearsoft.com>
2018-10-22 11:34:27 -0600

    Initial commit

---
+++ src/main/java/com/nearsoft/tbwlogistics/TbwLogisticsApplication.java
@@ -0,0 +1,12 @@
+package com.nearsoft.tbwlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbwLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbwLogisticsApplication.class, args);
+	}
+}
