44b66cccd51c9a77f17cba0aeb746f9fef3b9511
Rafa Manrique <rmanrique@nearsoft.com>
2018-11-13 09:19:12 -0700

    Update data.sql with DB data for rides

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,3 +18,19 @@ insert into daily_activity (name, description, date, duration_in_hours, place, a
   values ('Carnita asada', 'Nos reunimos a la llegada de los foraneos para una carne asada', CURRENT_DATE(), 4, 'La mansion', 0);
 insert into daily_activity (name, description, date, duration_in_hours, place, attendance_limit)
   values ('Team building day', 'Trabajamos en actividades de integracion e innovacion', CURRENT_DATE(), 8, 'Hotel X', 0);
+
+insert into ride_type (description) values ('going ride');
+insert into ride_type (description) values ('ride back');
+
+insert into ride (daily_activity_id, driver_id, available_seats, type) values (1, 4, 2, 1);
+insert into ride (daily_activity_id, driver_id, available_seats, type) values (1, 5, 2, 1);
+insert into ride (daily_activity_id, driver_id, available_seats, type) values (1, 5, 4, 2);
+
+insert into ride_passenger (ride_id, person_id) values (1, 1);
+insert into ride_passenger (ride_id, person_id) values (1, 2);
+insert into ride_passenger (ride_id, person_id) values (2, 3);
+insert into ride_passenger (ride_id, person_id) values (2, 6);
+insert into ride_passenger (ride_id, person_id) values (3, 1);
+insert into ride_passenger (ride_id, person_id) values (3, 2);
+insert into ride_passenger (ride_id, person_id) values (3, 3);
+insert into ride_passenger (ride_id, person_id) values (3, 6);
